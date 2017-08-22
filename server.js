b9a74ddb9759f2d98f9c9b64e082dbe3902948ca
jeffersonsam93 <30315299+jeffersonsam93@users.noreply.github.com>
2017-08-22 09:41:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -31,10 +31,10 @@ app.get('/article3',function(req,res){
 app.get('/test-db',function(req,res){
     pool.query('SELECT * FROM user',function(err,result){
         if(err){
-            res.status(500).send(err.toString())
+            res.status(500).send(err.toString());
         }
         else{
-            res.send(JSON.stringify(result))
+            res.send(JSON.stringify(result));
         }
     });
 });
