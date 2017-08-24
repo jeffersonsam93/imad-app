20cb93bb6aae39e06d85ddbf943db33d69c6f4a8
jeffersonsam93 <30315299+jeffersonsam93@users.noreply.github.com>
2017-08-24 12:19:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -62,7 +62,15 @@ app.post('/login',function(req,res){
             res.send(403).send("User/password Invalid");
             }
             else{
-                
+                var dbString=res.rows[0].password;
+                var salt=dbstring.split("$")[2];
+                var hashedpassword=hash(password,salt);
+                if(hashedpassword===dbString){
+                    res.send("Credentials correct");
+                }
+                else{
+                     res.send("User Successfully created:"+username);
+                }
             }
             res.send("User Successfully created:"+username);
         }
