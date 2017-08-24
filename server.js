feaa3425ebec381df2f7da352a7354916a9ee487
jeffersonsam93 <30315299+jeffersonsam93@users.noreply.github.com>
2017-08-24 14:57:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.post('/create-user',function(req,res){
 app.post('/login',function(req,res){
     var username=req.body.username;
     var password=req.body.password;
-    pool.query("SELECT * FROM userinfo where username=$1",[username],function(err,result){
+    pool.query("SELECT * FROM userinfo where username=$1",username,function(err,result){
         if(err){
             res.status(500).send(err.toString());
         }
