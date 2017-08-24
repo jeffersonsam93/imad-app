c54160a0fdf2e64c47e10d9f41fea8a8e96e5c59
jeffersonsam93 <30315299+jeffersonsam93@users.noreply.github.com>
2017-08-24 14:26:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,20 @@
 console.log('Loaded!');
+var submitbtn=document.getElementById("submit-btn");
+submitbtn.onclick=function(){
+    var request=new XMLHttpRequest();
+    var username=document.getElementById("username");
+    var password=document.getElementById("password");
+    request.onrequeststatechange=function(){
+      if(request.readyState===XMLHttpRequest.DONE){
+          if(request.status===200){
+              alert("logged in successfully");
+          }
+          else{
+              alert("Login failed");
+          }
+      }  
+    };
+    request.open("POST","http://jeffersonsam93.imad.hasura-app.io/",true);
+    request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringify({username:username,password:password}));
+};
