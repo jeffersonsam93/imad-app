d4fea212fb0662f8119fb4be4ff400142339bcf9
jeffersonsam93 <30315299+jeffersonsam93@users.noreply.github.com>
2017-08-24 10:30:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool=require('pg').Pool;
+var crypto=require('crypto');
 var config={
     user:'jeffersonsam93',
     database:'jeffersonsam93',
@@ -23,6 +24,14 @@ app.get('/article1',function(req,res){
 
 app.get('/article2',function(req,res){
     res.sendFile(path.join(__dirname, 'ui', 'artcle2.html'));
+});
+function hash(input,salt){
+    var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+    return  hashed.toString('hex');
+}
+app.get('/hash/:input',function(req,res){
+    var hashedString=hash(req.params.input,'randomstring');
+    res.send(hashedString)
 });
 
 app.get('/article3',function(req,res){
