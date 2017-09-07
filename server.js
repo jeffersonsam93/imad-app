4b55fa064b9aecc2a4f986d0f8f900e3d8b3d6ae
jeffersonsam93 <30315299+jeffersonsam93@users.noreply.github.com>
2017-09-07 15:29:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.post('/login',function(req,res){
         }
         else{
             if(result.rows.length===0){
-            res.send(403).send("User/password Invalid");
+            res.status(403).send("User/password Invalid");
             }
             else{
                 var dbString=result.rows[0].password;
@@ -76,7 +76,7 @@ app.post('/login',function(req,res){
                     res.send("Credentials correct");
                 }
                 else{
-                     res.send(403).send("User/password Invalid");
+                     res.status(403).send("User/password Invalid");
                 }
             }
         }
